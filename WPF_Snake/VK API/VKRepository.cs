76119692ac11ad88ab688a1091819b550d730272
Monpase <gsavlaev@mail.ru>
2016-03-20 20:49:38 +0300

    VK API

---
+++ WPF_Snake/VK API/VKRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_Snake.VK_API
+{
+    class VKRepository
+    {
+        string apiId;
+        string appId;
+        string token;
+
+        WebBrowser wb = new WebBrowser();
+        
+    }
+}
